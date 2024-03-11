2b852fd0a1ae7771a886a69c11e0d7555feffda8
lovushka <58827882+lovyshka@users.noreply.github.com>
2024-03-11 13:48:30 +0300

    Add files via upload

---
+++ subproc.c
@@ -0,0 +1,26 @@
+#include "total.h"
+
+
+int main(int argc, char * argv[]){
+    int fd_to_read, fd_to_write, cnt, sum = 0;
+    sscanf(argv[1], "%d", &fd_to_read);
+    sscanf(argv[2], "%d", &fd_to_write);
+    if (read(fd_to_read, &cnt, sizeof(int)) == -1){
+        printf("Error while reading from pipe\n");
+        return -1;
+    }
+    int tmp[cnt];
+    if (read(fd_to_read, tmp, sizeof(tmp)) == -1 || close(fd_to_read) == -1){
+        printf("Error while working withh pipe\n");
+        return -1;
+    }
+    for (int i = 0; i < cnt; i++) {
+        if (tmp[i] % 2 == 0) sum += tmp[i];
+    }
+
+    if (write(fd_to_write, &sum, sizeof(int)) == -1 || close(fd_to_write) == -1){
+        printf("Error while working writing in pipe\n");
+        return -1;
+    }
+    return 0;
+}
---
+++ total.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <ctype.h>
+
+
+void body(FILE * fp, int number_of_processes, int data_cnt);
+void check_and_reduce(int * number_of_processes, int arr_len);
+int check_argc(int argc);
+int check_data(int data_cnt);
+int lovit_cal(FILE * fp, int number_of_processes);
+int * divided_properly(int number_of_processes, int number_of_numbers);
+int from_string_to_int(char * arg);
+int get_number_of_input(FILE * fp);
+int work_with_processes(int number_of_process, int arr_len, int * arr);
+int get_total_sum(int number_of_process, int (* fd_from_ch_to_p)[2]);
